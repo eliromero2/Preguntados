7efb4f5f9a030f577be57b5ea281714df2435f06
eliromero2 <elianaromero002@gmail.com>
2023-10-12 22:53:17 -0300

    login no terminado
    (no anda)

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -13,4 +13,38 @@ class LoginController{
     public function list() {
         $this->render->printView('login');
     }
+
+    public function login(){
+        $data = [];
+
+        if(!empty($_SESSION['error'])){
+            $data["error"] = $_SESSION['error'];
+            unset( $_SESSION['error']);
+        }
+
+        $data['action'] = '/login/procesarLogin';
+        $data['submitText'] = 'Ingresar';
+        $this->render->printView('login', $data);
+
+
+    }
+
+    public function procesarLogin(){
+        if( empty($_POST['password'] ) || empty($_POST['user_name'] )  ){
+            $_SESSION["error"] = "Alguno de los campos era erroneo o vacio";
+            Redirect::to('/login');
+        }
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
+
+        $this->userModel->validarUsuario($user_name,$password);
+        Redirect::to('/home/list');
+    }
+
+    public function cerrarSesion(){//revisarrrrr
+        session_destroy();
+        echo('Se cerro la sesion');
+        Redirect::to('/home/list');
+    }
+
 }
--- model/userModel.php
+++ model/userModel.php
@@ -13,5 +13,39 @@ class userModel{
         $this->database->query($sql);
     }
 
+    public function buscarUsuario($user_name,$password){
+        $sql="SELECT * FROM users WHERE user_name = '$user_name' and password = '$password'";
+        Logger::info('Se econtro el usuario: ' . $sql);
+
+        $this->database->query($sql);
+
+}
+
+    public function validarUsuario($user_name,$password){
+        $sql="SELECT * FROM users WHERE user_name = '$user_name' and password = '$password'";
+        Logger::info('Se econtro el usuario: ' . $sql);
+
+        $resultado = $this->database->query($sql);
+
+        if (!$resultado || mysqli_num_rows($resultado) === 0) {
+            Logger::info('NO econtro el usuario: ' . $sql);
+            return false;
+        }
+        $user = mysqli_fetch_assoc($resultado);
+        mysqli_free_result($resultado);
+
+        // Compara la contraseña ingresada con la contraseña almacenada en la base de datos.
+        if ($password === $user['password']) {
+            Logger::info('La contraseña es correcta');
+            return true;
+
+        } else {
+            Logger::info('Contraseña incorrecta');
+            return false;
+        }
+    }
+
+
+
 
 }
